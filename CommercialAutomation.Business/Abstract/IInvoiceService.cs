10ccf6841b850f490571d166867ff9dd411fb4c0
oğuzhan cengiz <ozancengiz898@gmail.com>
2021-07-19 19:48:57 +0300

    19.07.2021

---
+++ CommercialAutomation.Business/Abstract/IInvoiceService.cs
@@ -0,0 +1,18 @@
+﻿using CommercialAutomation.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommercialAutomation.Business.Abstract
+{
+    public interface IInvoiceService
+    {
+        List<Invoice> GetAll();
+        Invoice GetById(int id);
+        void Add(Invoice ınvoice);
+        void Update(Invoice ınvoice);
+        void Delete(Invoice ınvoice);
+    }
+}
---
+++ CommercialAutomation.Business/Concrete/InvoiceManager.cs
@@ -0,0 +1,46 @@
+﻿using CommercialAutomation.Business.Abstract;
+using CommercialAutomation.DataAccess.Abstract;
+using CommercialAutomation.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommercialAutomation.Business.Concrete
+{
+    public class InvoiceManager : IInvoiceService
+    {
+        IInvoiceDal _ınvoiceDal;
+
+        public InvoiceManager(IInvoiceDal ınvoiceDal)
+        {
+            _ınvoiceDal = ınvoiceDal;
+        }
+
+        public void Add(Invoice ınvoice)
+        {
+            _ınvoiceDal.Add(ınvoice);
+        }
+
+        public void Delete(Invoice ınvoice)
+        {
+            _ınvoiceDal.Delete(ınvoice);
+        }
+
+        public List<Invoice> GetAll()
+        {
+            return _ınvoiceDal.GetAll();
+        }
+
+        public Invoice GetById(int id)
+        {
+            return _ınvoiceDal.GetById(x => x.InvoiceId == id);
+        }
+
+        public void Update(Invoice ınvoice)
+        {
+            _ınvoiceDal.Update(ınvoice);
+        }
+    }
+}
