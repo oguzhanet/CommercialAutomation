7dd3badc4d0e749ecb5734af9a93efccd225a831
oğuzhan cengiz <ozancengiz898@gmail.com>
2021-07-19 20:19:39 +0300

    19.07.2021

---
+++ CommercialAutomation.MvcWebUI/Controllers/InvoiceController.cs
@@ -0,0 +1,26 @@
+﻿using CommercialAutomation.Business.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CommercialAutomation.MvcWebUI.Controllers
+{
+    public class InvoiceController : Controller
+    {
+        // GET: Invoice
+        IInvoiceService _ınvoiceService;
+
+        public InvoiceController(IInvoiceService ınvoiceService)
+        {
+            _ınvoiceService = ınvoiceService;
+        }
+
+        public ActionResult Index()
+        {
+            var result = _ınvoiceService.GetAll();
+            return View(result);
+        }
+    }
+}
