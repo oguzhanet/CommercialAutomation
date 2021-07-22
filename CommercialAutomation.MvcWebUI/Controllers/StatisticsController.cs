1f6657b5cf9e364559d166d5da1cd38fa12762d1
oğuzhan cengiz <ozancengiz898@gmail.com>
2021-07-22 19:43:29 +0300

    22.07.2021

--- CommercialAutomation.MvcWebUI/Controllers/StatisticsController.cs
+++ CommercialAutomation.MvcWebUI/Controllers/StatisticsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommercialAutomation.DataAccess.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,8 +10,27 @@ namespace CommercialAutomation.MvcWebUI.Controllers
     public class StatisticsController : Controller
     {
         // GET: Statistics
+        Context _context;
+
+        public StatisticsController(Context context)
+        {
+            _context = context;
+        }
+
         public ActionResult Index()
         {
+            var result1 = _context.Customers.Count().ToString();
+            ViewBag.result1 = result1;
+
+            var result2 = _context.Employees.Count().ToString();
+            ViewBag.result2 = result2;
+
+            var result3 = _context.Categories.Count().ToString();
+            ViewBag.result3 = result3;
+
+            var result4 = _context.Products.Count().ToString();
+            ViewBag.result4 = result4;
+
             return View();
         }
     }
