1341471456b8a0850ef90d654c629b675be51007
oğuzhan cengiz <ozancengiz898@gmail.com>
2021-07-30 15:48:32 +0300

    30.07.2021

--- CommercialAutomation.MvcWebUI/Controllers/ToDoListController.cs
+++ CommercialAutomation.MvcWebUI/Controllers/ToDoListController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CommercialAutomation.Business.Concrete;
+using CommercialAutomation.DataAccess.Concrete;
+using CommercialAutomation.DataAccess.Concrete.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +12,51 @@ namespace CommercialAutomation.MvcWebUI.Controllers
     public class ToDoListController : Controller
     {
         // GET: ToDoList
+        ToDoListManager toDoListManager = new ToDoListManager(new EfToDoListDal());
+        Context _context;
+
+        public ToDoListController(Context context)
+        {
+            _context = context;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var result = toDoListManager.GetAll();
+
+            var result1 = _context.SaleMovements.Count(x => x.SaleDate == DateTime.Today).ToString();
+            if (result1 !=null)
+            {
+                ViewBag.result1 = result1;
+            }
+            else
+            {
+                ViewBag.result1 = 0;
+            }
+            
+            var result2 = _context.Products.Count(x => x.UnitsInStock <= 10).ToString();
+            ViewBag.result2 = result2;
+
+            var result3 = _context.Customers.Count().ToString();
+            ViewBag.result3 = result3;
+
+            var result4 = (from x in _context.Customers select x.CustomerCity).Distinct().Count().ToString();
+            ViewBag.result4 = result4;
+
+            return View(result);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var result = toDoListManager.GetById(id);
+
+            if (result.ToDoListStatus == false)
+            {
+                result.ToDoListStatus = true;
+            }
+
+            toDoListManager.Update(result);
+            return RedirectToAction("Index");
         }
     }
 }
