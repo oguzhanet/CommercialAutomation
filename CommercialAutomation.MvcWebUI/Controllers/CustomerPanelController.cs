88c1df66fd9c47fd71c048ed8fa7e02e2a72bd74
oğuzhan cengiz <ozancengiz898@gmail.com>
2021-07-31 14:39:04 +0300

    31.07.2021

---
+++ CommercialAutomation.MvcWebUI/Controllers/CustomerPanelController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CommercialAutomation.MvcWebUI.Controllers
+{
+    [Authorize]
+    public class CustomerPanelController : Controller
+    {
+        // GET: CustomerPanel
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
